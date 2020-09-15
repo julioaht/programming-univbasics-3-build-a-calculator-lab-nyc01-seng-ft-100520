9bd0c7aae2dd5c1d3b835693dba41759c6657f7d
julio hernandez <julioandres72@gmail.com>
2020-09-15 18:53:53 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -14,13 +14,16 @@ def division(num1, num2)
 end
 
 def multiplication(num1, num2)
-
+  sum = num1 * num2
+  sum
 end
 
 def modulo(num1, num2)
-
+  sum = num1 % num2
+  sum
 end
 
 def square_root(num)
-
+  sum = num **
+  sum
 end
